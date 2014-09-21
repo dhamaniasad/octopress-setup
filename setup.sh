0c49277df2ff833bdbeb0b97aac826a300d36f3b
Asad Dhamani <dhamaniasad@gmail.com>
2014-09-21 19:12:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,43 @@ if [[ $EUID -ne 0 ]]; then
 echo "This script must be run as root" 1>&2
    exit 1
 fi
+
+function install_git {
+   check_install git git
+}
+
+function install_rvm {
+   curl -L https://get.rvm.io | bash -s stable --ruby
+}
+
+function install_ruby {
+   rvm install 1.9.3
+}
+
+function switch_193 {
+   rvm use 1.9.3
+}
+
+function ruby_gems {
+   rvm rubygems latest
+}
+
+function git_clone {
+   git clone git://github.com/imathis/octopress.git octopress
+}
+
+function chdir {
+   cd octopress
+}
+
+function install_bundler {
+   gem install bundler
+}
+
+function install_bundle {
+   bundle install
+}
+
+function default_theme {
+   rake install
+}
