0bbf664a6ccaaefa96f3d966b61700ca8e23862c
Asad Dhamani <dhamaniasad@gmail.com>
2014-09-21 19:27:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,55 +5,46 @@ echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-function install_git {
-   check_install git git
-}
+# Install git
 
-function install_rvm {
-   curl -L https://get.rvm.io | bash -s stable --ruby
-}
+apt-get -y install git
 
-function install_ruby {
-   rvm install 1.9.3
-}
+# Install RVM
 
-function switch_193 {
-   rvm use 1.9.3
-}
+curl -L https://get.rvm.io | bash -s stable --ruby
 
-function ruby_gems {
-   rvm rubygems latest
-}
+# Install Ruby 1.9.3
 
-function git_clone {
-   git clone git://github.com/imathis/octopress.git octopress
-}
+rvm install 1.9.3
 
-function chdir {
-   cd octopress
-}
+# Switch to Ruby 1.9.3
 
-function install_bundler {
-   gem install bundler
-}
+rvm use 1.9.3
 
-function install_bundle {
-   bundle install
-}
+# Install Rubygems
 
-function default_theme {
-   rake install
-}
+rvm rubygems latest
 
-*)
-   install_git
-   install_rvm
-   install_ruby
-   switch_193
-   ruby_gems
-   git_clone
-   chdir
-   install_bundler
-   install_bundle
-   default_theme
-   ;;
+# Install node.js
+
+apt-get -y install nodejs
+
+# Clone Octopress git repository
+
+git clone git://github.com/imathis/octopress.git octopress
+
+# Switch to octopress dir
+
+cd octopress
+
+# Install bundler
+
+gem install bundler
+
+# Install bundle
+   
+bundle install
+
+# Install default theme  
+
+rake install
